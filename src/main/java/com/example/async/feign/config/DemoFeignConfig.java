84a3bdfd26555c07781dae24abd41352fbeb4163
nakgyeom <himina1227@gmail.com>
2023-08-04 16:38:16 +0900

    - Error Decoder

--- src/main/java/com/example/async/feign/config/DemoFeignConfig.java
+++ src/main/java/com/example/async/feign/config/DemoFeignConfig.java
@@ -1,5 +1,6 @@
 package com.example.async.feign.config;
 
+import com.example.async.feign.decoder.DemoFeignErrorDecoder;
 import com.example.async.feign.intercepter.DemoFeignInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,4 +12,10 @@ public class DemoFeignConfig {
     public DemoFeignInterceptor feignInterceptor() {
         return DemoFeignInterceptor.of();
     }
+
+    @Bean
+    public DemoFeignErrorDecoder DemoErrorDecoder() {
+        return new DemoFeignErrorDecoder();
+    }
+
 }
